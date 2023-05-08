7fa878897fd955a060a2dcec97a6bb73bbad2bc4
Rgop <86734401+Rgop@users.noreply.github.com>
2023-05-08 14:15:09 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -62,7 +62,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="🔂 ᴛʀʏ ᴀɢᴀɪɴ",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
@@ -80,7 +80,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="🔂 ᴛʀʏ ᴀɢᴀɪɴ",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
@@ -99,7 +99,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="🔂 ᴛʀʏ ᴀɢᴀɪɴ",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
